d609bdc581c9bbfc94847c171bd6073a5747ac19
canbayrams <74656337+canbayrams@users.noreply.github.com>
2020-11-30 15:40:20 +0300

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -31,10 +31,10 @@ def predict():
     # Check the output values and retrive the result with html tag based on the value
     if output == 1:
         return render_template('Heart Disease Classifier.html', 
-                               result = 'The patient is not likely to have heart disease!')
+                               result = 'Hastanın kalp hastalığı olması muhtemel değildir!')
     else:
         return render_template('Heart Disease Classifier.html', 
-                               result = 'The patient is likely to have heart disease!')
+                               result = 'Hastanın kalp hastalığı olması muhtemeldir')
 
 if __name__ == '__main__':
 #Run the application
